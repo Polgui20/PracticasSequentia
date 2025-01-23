ecd3ed5317dadef71b7174151313d16a2c58a7d8
Pol Guillén Noguera <162967193+Polgui20@users.noreply.github.com>
2025-01-23 15:56:30 +0100

    Add files via upload

---
+++ app/android/index.php
@@ -0,0 +1,5 @@
+<html>
+<?php  
+header("Location: https://play.google.com/store/apps/details?id=com.unow.Sequentia", true, 301);  
+exit();  
+?> 
---
+++ app/ios/index.php
@@ -0,0 +1,5 @@
+<html>
+<?php  
+header("Location: https://apps.apple.com/es/app/sequentia/id6448124358", true, 301);  
+exit();  
+?> 
